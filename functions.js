3d9e8dd8358fe4becc5a49f3797b7c43d5de4dd8
lucaszw <lucas.zeer@gmail.com>
2017-11-22 09:50:55 -0500

    migrate tasks for main microdrop repo

---
+++ functions.js
@@ -0,0 +1,72 @@
+module.exports = {};
+
+module.exports.clearDatabase = () => {
+  return new Promise((resolve, reject) => {
+    del([path.resolve("db")]).then(paths => {
+        resolve('Deleted files and folders:\n', paths.join('\n'));
+    });
+  });
+}
+
+module.exports.installMicrodrop = async () {
+  return await callCommand("npm install");
+}
+
+module.exports.installDeps = async(mode) {
+  var deps = getMicrodropDeps();
+  let names;
+
+  if (mode == "production") {
+    names = _.keys(deps);
+  }
+  if (mode == "developer") {
+    names = _.map(_.values(deps), (d) => path.resolve("packages", d));
+  }
+
+  return await callCommand(`npm i ${names.join(" ")}`);
+}
+
+module.exports.uninstallDeps = async () => {
+  var deps = _.keys(getMicrodropDeps());
+  return await callCommand(`npm uninstall ${deps.join(" ")}`);
+}
+
+module.exports.callCommand = (command) => {
+  return new Promise((resolve, reject) => {
+    var child = spawn(command, {stdio: 'inherit', shell: true});
+    child.on('exit', function (code) {
+      resolve('child process exited with code ' + code.toString());
+    });
+  });
+}
+
+module.exports.getMicrodropDeps = () => {
+  var packageJSON = require(path.resolve('package.json'));
+  return packageJSON.microdropDependencies;
+}
+
+module.exports.getPackages = () => {
+  var files = fs.readdirSync(path.resolve("packages"));
+  var packages = [];
+  for (const [i, package] of files.entries()) {
+    var packageDir = path.resolve("packages", package);
+    var stats = fs.statSync(packageDir);
+    if (stats.isDirectory()) {
+      if (fs.existsSync(path.resolve(packageDir,'package.json'))) {
+        packages.push(packageDir);
+      }
+    }
+  }
+  return packages;
+}
+
+module.exports.getPlugins = () => {
+  var packages = getPackages();
+  var plugins = [];
+  for (const [i, package] of packages.entries()) {
+    if (fs.existsSync(path.resolve(package,'microdrop.json'))) {
+      plugins.push(package);
+    }
+  }
+  return plugins;
+}
---
+++ main.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const _ = require('lodash');
+
+_.extend(global, require('./functions'));
+
+gulp.task('get:packages', () => {
+  console.log(getPackages());
+});
+
+gulp.task('get:plugins', function() {
+  console.log(getPlugins());
+});
+
+gulp.task('get:deps', function() {
+  console.log(getMicrodropDeps());
+});
+
+gulp.task('uninstall:deps', async function() {
+  return await uninstallDeps();
+});
+
+gulp.task('install:production', async function() {
+  return await installDeps("production");
+});
+
+gulp.task('install:developer', async function() {
+  return await installDeps("developer");
+});
+
+gulp.task('mode:production', async function() {
+  await uninstallDeps();
+  return await installDeps("production");
+});
+
+gulp.task('mode:developer', async function() {
+  await uninstallDeps();
+  return await installDeps("developer");
+});
+
+gulp.task('install:microdrop', async function() {
+  return await installMicrodrop();
+});
+
+gulp.task('install:all', async function() {
+  const cmd = path.resolve("node_modules/.bin/npm-recursive-install");
+  await installMicrodrop();
+  await callCommand(cmd);
+});
+
+gulp.task('start', async function() {
+  await callCommand("node index.js");
+});
+
+gulp.task('reset:db', async function() {
+  return await clearDatabase();
+});
+
+gulp.task('git:add', async function() {
+
+  await callCommand('git add package-lock.json');
+  await callCommand('git add -p');
+  await callCommand('git commit');
+
+});
